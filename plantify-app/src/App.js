a6ce9d80af38b43eaa8cbf6bea50f3676740feb7
Svetlana <svetlana.esina.360@gmail.com>
2023-06-20 10:59:25 -0400

    Added basic skeleton for navigation

--- plantify-app/src/App.js
+++ plantify-app/src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './components/pages/Home';
+import AboutUs from './components/pages/AboutUs';
+import MainNavbar from './components/MainNavbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <MainNavbar />
+
+          <Routes>
+            <Route path='/' element = {<Home />} />
+            <Route path='/AboutUs' element = {<AboutUs />} />
+
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
