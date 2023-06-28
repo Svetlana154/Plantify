076e098434699b6fda74ec283c66e9b1b04c388a
Svetlana154 <svetlana.esina.360@gmail.com>
2023-06-27 23:51:17 -0400

    Added FAQ section

--- plantify-app/src/components/pages/FAQ.js
+++ plantify-app/src/components/pages/FAQ.js
@@ -1,9 +1,32 @@
+import Container from "react-bootstrap/esm/Container";
+import Accordion from "react-bootstrap/esm/Accordion";
+import "../../styles/FAQ.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleQuestion } from "@fortawesome/free-solid-svg-icons";
+import faqData from "../../data/faq.json";
+
 function FAQ() {
+  const accordionItems = [];
+
+  faqData.forEach(item => {
+    accordionItems.push(
+      <Accordion.Item eventKey={item.key}>
+        <Accordion.Header>{item.header}</Accordion.Header>
+        <Accordion.Body>
+          {item.body}
+        </Accordion.Body>
+      </Accordion.Item>
+      );
+  });
+
   return (
-    <div>
-      <h1>Hello, world</h1>
-      <p>FAQ</p>
-    </div>
+    <Container className="faq">
+      <FontAwesomeIcon icon={faCircleQuestion} size='2xl' className="faq-icon"/>
+      <h1>Frequently Asked Questions</h1>
+      <Accordion variant="dark" className="faq-content" alwaysOpen>
+        {accordionItems}
+      </Accordion>
+    </Container>
   );
 }
 
